13949019f05add41c2ae4372b257be3a7abc0a49
Bubble Wrap <vishalpandey000090@gmail.com>
2025-11-26 02:05:49 +0530

    Update getResult endpoint to return uploaded file path

--- backend/frontend/vite.config.js
+++ backend/frontend/vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    headers:{
-      "Cross-Origin-Embedder-Policy":"require-crop",
-      "Cross-Origin-openner-Policy":"same-origin"
-    }
-    ,
+
+  server: {
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp"
+    },
+
     proxy: {
       '/cdn': {
         target: 'https://unpkg.com',
@@ -17,6 +17,5 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/cdn/, '')
       }
     }
-
   }
 })
